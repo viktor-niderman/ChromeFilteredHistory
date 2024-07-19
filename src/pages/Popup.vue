05ec6c24a21e444a428b18477da9575b20c82369
niderman <viktor.niderman@gmail.com>
2024-07-19 14:32:20 +0100

    start

--- src/pages/Popup.vue
+++ src/pages/Popup.vue
@@ -1,10 +1,35 @@
 <script setup>
+import { ref } from 'vue'
+
+let origin = ref('');
+
+const clear = () => {
+//   chrome.browsingData.remove({
+//     "since": 0,
+//     "origins": ['https://' + origin, 'http://' + origin]
+//   }, {
+//     appcache: true,
+//     cache: true,
+//   }, () => {});
+
+  chrome.history.search({text: origin.value, startTime: 0}, function(results) {
+    for (let item of results) {
+      chrome.history.deleteUrl({url: item.url}, function() {
+        console.log(`Deleted: ${item.url}`);
+      });
+    }
+  });
+}
+
 
 </script>
 
 <template>
   <div>
       <h1>Filtered History</h1>
+    <p>Enter Origin:</p>
+    <input v-model="origin" placeholder="google.com">
+    <button @click="clear()">Ok</button>
   </div>
 </template>
 
